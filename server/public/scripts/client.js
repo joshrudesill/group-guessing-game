7c53f7693e6398d42c5837712295d7817cf87347
Beth Tracy <bntracy@gmail.com>
2024-01-25 14:11:31 -0600

    working on GET request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,11 +18,37 @@ function submitGuesses(event) {
     data: playerGuesses
   }).then(function(response){
     console.log('response is good', response);
-    // check to see if someone got it right
-
-    // display the ids
+    document.getElementById('p1').value = '';
+    document.getElementById('p2').value = '';
+    document.getElementById('p3').value = '';
+    getHistory();
   }).catch(function(error){
     console.log('error', error);
   });
 
+}
+
+function getHistory() {
+  axios({
+    method: 'GET',
+    url: '/game'
+  }).then(function(response){
+    console.log('response is good', response);
+    // compare guesses
+    // get them in an array
+    // need to look at the most recent row
+    let allGuesses = response.data; // this will be an array
+    let currentGuesses = allGuesses[allGuesses.length - 1]; // this will be an object
+    let actualNumber = currentGuesses.actualNumber;
+    let p1 = currentGuesses.guesses.p1;
+    let p2 = currentGuesses.guesses.p2;
+    let p3 = currentGuesses.guesses.p3;
+    console.log('p1', p1);
+    console.log('p2', p2);
+    console.log('p3', p3);
+    console.log('actualNumber', actualNumber);
+    // change the DOM
+  }).catch(function(error){
+    console.log('error', error);
+  });
 }

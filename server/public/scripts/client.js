88dd4b023ea5d86390a92f5b6d071cc542313848
Beth Tracy <bntracy@gmail.com>
2024-01-25 13:13:47 -0600

    sends guesses to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,27 @@ function onReady() {
   console.log("JavaScript is loaded!")
 }
 
-onReady()
+onReady();
+
+function submitGuesses(event) {
+  event.preventDefault();
+  let playerGuesses = {
+    p1: document.getElementById('p1').value,
+    p2: document.getElementById('p2').value,
+    p3: document.getElementById('p3').value
+  };
+
+  axios({
+    method: 'POST',
+    url: '/game',
+    data: playerGuesses
+  }).then(function(response){
+    console.log('response is good', response);
+    // check to see if someone got it right
+
+    // display the ids
+  }).catch(function(error){
+    console.log('error', error);
+  });
+
+}

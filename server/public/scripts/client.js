f5c7b485d323814d02ed5a1235b0b7edb0bbe03f
Beth Tracy <bntracy@gmail.com>
2024-01-25 14:44:38 -0600

    doing some comparing

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -47,7 +47,49 @@ function getHistory() {
     console.log('p2', p2);
     console.log('p3', p3);
     console.log('actualNumber', actualNumber);
+
+    // need to actually do things with the guesses here
+    let compare1 = '';
+    let compare2 = '';
+    let compare3 = '';
+    if (p1 > actualNumber) {
+      compare1 = 'HIGHER';
+    } else if (p1 < actualNumber) {
+      compare1 = 'LOWER';
+    } else {
+      compare1 = 'YOU WIN!';
+    }
+
+    if (p2 > actualNumber) {
+      compare2 = 'HIGHER';
+    } else if (p2 < actualNumber) {
+      compare2 = 'LOWER';
+    } else {
+      compare2 = 'YOU WIN!';
+    }
+
+    if (p3 > actualNumber) {
+      compare3 = 'HIGHER';
+    } else if (p3 < actualNumber) {
+      compare3 = 'LOWER';
+    } else {
+      compare3 = 'YOU WIN!';
+    }
+
     // change the DOM
+    document.getElementById('details').innerHTML = '';
+    for (let i in allGuesses) {
+      document.getElementById('details').innerHTML += `
+      <tr>
+        <td>Round: ${Number(i) + 1}</td>
+        <td>p1: ${allGuesses[i].guesses.p1} <span>${compare1}</span></td>
+        <td>p2: ${allGuesses[i].guesses.p2} <span>${compare2}</span></td>
+        <td>p3: ${allGuesses[i].guesses.p3} <span>${compare3}</span></td>
+      </tr>
+      `;
+    }
+
+
   }).catch(function(error){
     console.log('error', error);
   });
